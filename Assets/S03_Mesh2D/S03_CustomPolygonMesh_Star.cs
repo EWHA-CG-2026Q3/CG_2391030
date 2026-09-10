3bf517f0ee32fd5fe456dd0262f6f704b04fbcb8
cnh0922-hash <cnh0922@ewhain.net>
2026-09-10 19:22:25 +0900

    as02

--- Assets/S03_Mesh2D/S03_CustomPolygonMesh_Star.cs
+++ Assets/S03_Mesh2D/S03_CustomPolygonMesh_Star.cs
@@ -1,24 +1,32 @@
 ﻿  using UnityEngine;
 
   [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-  public class S03_CustomPolygonMesh_Square : MonoBehaviour
+  public class S03_CustomPolygonMesh_Star : MonoBehaviour
   {
       void Start()
       {
           // TODO 1: 원하는 다각형의 정점 좌표를 채우세요 (최소 4개)
           Vector3[] vertices = new Vector3[]
           {
-              new Vector3(0f, 1f, 0f),
-              new Vector3(1f, 1f, 0f),
-              new Vector3(1f, 0f, 0f),
-              new Vector3(0f, 0f, 0f),
+              new Vector3(0f, 0.7f, 0f), //0
+              new Vector3(0.9f, 0f, 0f), //1
+              new Vector3(-0.9f, 0f, 0f), //2
+              new Vector3(-0.5f, -1f, 0f), //3
+              new Vector3(0.5f, -1f, 0f), //4
+
+              new Vector3(-0.5f, 0.5f, 0f), //5
+              new Vector3(0.5f, 0.5f, 0f), //6
+              new Vector3(-0.2f, -0.5f, 0f), //7
+              new Vector3(0.2f, -0.5f, 0f), //8
+              new Vector3(0f, -0.5f, 0f), //9
           };
 
           // TODO 2: 정점 3개씩 묶어 삼각형들을 구성하세요
           int[] triangles = new int[]
           {
-              0, 1, 2,
-              0, 2, 3,
+              1, 2, 9,
+              0, 3, 8,
+              0, 7, 4,
           };
 
           Mesh mesh = new Mesh();
